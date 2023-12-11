290430059a24dd603fee66c1ef51182ea12f59b7
TaylorTWBrown <taylorami@hotmail.com>
2023-12-11 14:26:18 -0500

    Update code

--- src/main/java/com/example/lab6/HelloApplication.java
+++ src/main/java/com/example/lab6/HelloApplication.java
@@ -1,23 +1,86 @@
 package com.example.lab6;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-import java.io.IOException;
+class Account {
+    private double balance;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public Account(double initialBalance) {
+        this.balance = initialBalance;
     }
 
+    public synchronized void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposit: " + amount + ", New Balance: " + balance);
+    }
+
+    public synchronized void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawal: " + amount + ", New Balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds for withdrawal.");
+        }
+    }
+
+    public synchronized double getBalance() {
+        return balance;
+    }
+}
+
+class Transaction implements Runnable {
+    private Account account;
+    private String transactionType;
+    private double amount;
+
+    public Transaction(Account account, String transactionType, double amount) {
+        this.account = account;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        if ("deposit".equals(transactionType)) {
+            account.deposit(amount);
+        } else if ("withdraw".equals(transactionType)) {
+            account.withdraw(amount);
+        }
+    }
+}
+
+public class HelloApplication {
     public static void main(String[] args) {
-        launch();
+        // Create an account with initial balance
+        Account account = new Account(1000.0);
+
+        // Create a list of transactions
+        ArrayList<Runnable> transactions = new ArrayList<>();
+        transactions.add(new Transaction(account, "deposit", 200.0));
+        transactions.add(new Transaction(account, "withdraw", 300.0));
+        transactions.add(new Transaction(account, "withdraw", 800.0));
+
+        // Create an ExecutorService with a fixed thread pool size
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        // Execute the transactions using the ExecutorService
+        for (Runnable transaction : transactions) {
+            executor.execute(transaction);
+        }
+
+        // Shutdown the executor
+        executor.shutdown();
+
+        try {
+            // Wait for all threads to finish
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Balance: " + account.getBalance());
     }
 }
